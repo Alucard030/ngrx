dfe330748d40d21b2483816bf4033e848e20baca
Akshay Hrithik <akshay.hrithik2@mindtree.com>
2021-01-06 23:34:43 +0530

    ngrx demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Post } from './model/post.model';
+import * as PostActions from './actions/post.action'
+
+
+
+interface AppState {
+  post: Post;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngrx-demo';
+
+  post: Observable<Post>// emits value of type Post
+
+  text: string; /// form input val
+
+  constructor(private store: Store<AppState>) {
+    this.post = this.store.select('post')
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text) )
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset())
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote())
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote())
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { postReducer } from './reducers/post.reducers';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({post:postReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
